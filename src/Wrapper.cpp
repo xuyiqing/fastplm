75fa7b1df49dc4a52db66fae24ca65aeb72de048
Minsheng Liu <lambda@liu.ms>
2018-04-15 14:02:04 -0700

    Remove old GPSolver implementation.

--- src/Wrapper.cpp
+++ src/Wrapper.cpp
@@ -1,7 +1,6 @@
 #include "CrushQueue.h"
 #include "FixedEffects.h"
 #include "FixedEffectModel.h"
-#include "GPSolver.h"
 
 using Rcpp::List;
 using Rcpp::XPtr;
@@ -37,16 +36,4 @@ List SolveFixedEffects(arma::mat data, SEXP wrappedFixedEffects, std::size_t cor
 
     auto fixedEffects = XPtr<const FixedEffects>(wrappedFixedEffects);
     return FixedEffectModel::solve(data, *fixedEffects);
-}
-
-// [[Rcpp::export()]]
-List solveGP(arma::vec Y, arma::mat X, arma::mat tois, arma::mat iots, bool isBalanced) {
-    auto timeCount = tois.n_rows, indivCount = iots.n_rows;
-    arma::mat Y_(Y.memptr(), timeCount, indivCount, false);
-    arma::cube X_(X.memptr(), timeCount, indivCount, X.n_cols, false);
-    GPSolver solver(X_, Y_, tois, iots, isBalanced);
-    
-    List result;
-    result["coefficients"] = solver.compute();
-    return result;
 }
